98d23a304ae67e47a8ba7afb3a218914a8439a70
Susan Brown <brown684408@gmail.com>
2022-10-26 10:29:18 -0500

    add bootstrap and styling

--- src/teamTemplate.js
+++ src/teamTemplate.js
@@ -7,8 +7,8 @@ const renderTeam = (team) => {
     //this will create the Manager employee card
     const renderManager = manager => {
         return `
-        <div class= "team-cards">
-        <div class= "employee-card">
+        <div class= "team-cards col-12 d-flex justify-content-center">
+        <div class= "employee-card "d-flex p-2 bd-highlight"">
             <div class="card-header">
                 <h2 class="card-header">${manager.getName()}</h2>
                 <h3 class="card-header">${manager.getRole()}</h3>
@@ -37,7 +37,7 @@ const renderTeam = (team) => {
                 <ul class="employee-list">
                     <li class="employee-list-item">ID: ${engineer.getId()}</li>
                     <li class="employee-list-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}<a/></li>
-                    <li class="employee-list-item">Office Number: ${engineer.getGithub()}</li>
+                    <li class="employee-list-item">Github Username: ${engineer.getGithub()}</li>
                 </ul>
 
             </div>
@@ -57,12 +57,12 @@ const renderTeam = (team) => {
                 <ul class="employee-list">
                     <li class="employee-list-item">ID: ${intern.getId()}</li>
                     <li class="employee-list-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}<a/></li>
-                    <li class="employee-list-item">Office Number: ${intern.getSchool()}</li>
+                    <li class="employee-list-item">School: ${intern.getSchool()}</li>
                 </ul>
 
             </div>
         </div>
-        </div>
+
     `}
 
    
@@ -100,8 +100,29 @@ module.exports = (team) => {
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 <title>Team Profile</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css" />
 </head>
-<body>${renderTeam(team)}</body>
+<body>
+
+    <div>
+        <div class="row">
+            <div class="col-12 jumbotron mb-5 team-heading">
+                <h1 class="text-center">Team Profile</h1>
+            </div>
+        </div>
+    </div>
+    
+            <div class="employee-cards d-inline-flex">
+
+                ${renderTeam(team)}
+
+            </div>
+    
+
+</body>
+
     `
 }
+
+//<body>${renderTeam(team)}</body>
